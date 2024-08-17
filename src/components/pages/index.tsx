3f0d57e6e63adecab8205d4d7e5ba080f2a6fc5f
Youngmentor <olaleresuliton43@gmail.com>
2024-08-17 11:44:03 +0100

    added some modification to the store page

--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -1,13 +1,14 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useParams } from "react-router-dom";
 import Bottom from "./bottomNav";
 import Layout from "./layout";
 import SideBar from "./sidebar";
 import Logo from '../../assets/LOGO.svg'
 const Home = () => {
+    const {id} = useParams()
     const location = useLocation();
     const isDetailsPage = /\/home\/details\/\d+/.test(location.pathname);
     const isQuicksPage = location.pathname === '/home/quicks';
-
+    const isCommentPage = location.pathname === `/home/store/${id}`;
     return (
         <div className="w-[100%] p-[0px] dark:bg-black max-w-fill">
             <div className="w-[100%] h-[10vh] bg-black flex items-center justify-center fixed top-0 left-0 z-50 p-2">
@@ -23,10 +24,10 @@ const Home = () => {
                 </div>
             </div>
             <div className="w-[100%] flex h-[90vh] ">
-                <div className={isDetailsPage || isQuicksPage ? "hidden" : "w-[15%] h-[100%] bg-slate-50 max-[650px]:hidden mt-[60px]  "}>
+                <div className={isDetailsPage || isQuicksPage || isCommentPage ? "hidden" : "w-[15%] h-[100%] bg-slate-50 max-[650px]:hidden mt-[60px]  "}>
                     <SideBar />
                 </div>
-                <div className={isDetailsPage || isQuicksPage ? 'w-[100%]' : "w-[85%] flex flex-col items-center justify-center h-[100%] max-[650px]:w-[100%] "}>
+                <div className={isDetailsPage || isQuicksPage || isCommentPage ? 'w-[100%]' : "w-[85%] flex flex-col items-center justify-center h-[100%] max-[650px]:w-[100%] "}>
                     <div className="w-[100%] h-[92%] mt-[60px] max-[650px]:p-[0px] ">
                         <Layout />
                     </div>
--- src/components/pages/store/index.tsx
+++ src/components/pages/store/index.tsx
@@ -39,37 +39,24 @@ const Store = () => {
     }
     return (
         <div className="mt-[30px] w-[100%] flex items-center justify-center flex-col gap-[20px]">
-            <div className="w-[95%] h-[auto] p-[2%] shadow-lg shadow-grey-500/50 bg-slate-50  dark:bg-[#1D1C1C] dark:shadow-white-500/50 rounded-[16px]  max-[650px]:w-[90%] max-[650px]:p-[2%]">
+            <div className="w-[50%] mt-[50px] h-[auto] p-[2%] shadow-lg shadow-grey-500/50 bg-slate-50  dark:bg-[#1D1C1C] dark:shadow-white-500/50 rounded-[16px]  max-[650px]:w-[90%] max-[650px]:p-[2%]">
                 <div className="w-[60%] flex flex-col gap-[10px] max-[650px]:w-[100%] max-[650px]:items-center ">
                     <div className=" w-[100%] flex items-center gap-5 max-[650px]:flex-col">
-                        <span className="flex flex-col items-center gap-[10px] relative">
+                        <span className="flex w-[70%] flex-col items-center gap-[10px] relative">
                             <img src={AdminInfo?.image} alt="" className="w-[150px] h-[150px] rounded-[100%] object-cover max-[650px]:w-[80px] max-[650px]:h-[80px] " />
-                            <p className="text-[10px] font-bold hidden max-[650px]:flex ">{AdminInfo?.profession}</p>
+                            <p className="text-[15px] font-bold hidden max-[650px]:flex ">{AdminInfo?.profession}</p>
                         </span>
-                        <span className="h-[100px] w-[50%] gap-2 max-[650px]:w-[100%] max-[650px]:flex max-[650px]:items-center max-[650px]:flex-col">
-                            <p className="text-clamp">@{AdminInfo?.business_name}</p>
+                        <span className="h-[100px] w-[100%] gap-5 max-[650px]:w-[100%] max-[650px]:flex max-[650px]:items-center max-[650px]:flex-col">
+                            <p className="text-clamp text-[25px] mb-[10px]">@{AdminInfo?.business_name}</p>
                             <p className="text-[12px] max-[650px]:text-center">{AdminInfo?.bio}</p>
                         </span>
                     </div>
                     <div className="w-[100%] flex  items-center ">
                         <p className="text-[12px] font-bold max-[650px]:hidden ">{AdminInfo?.profession}</p>
                     </div>
-                    {/* <div className="flex justify-between w-[35%] max-[650px]:w-[70%] text-[16px] p-[10px]">
-                        <span className="flex gap-[5px] items-center">
-                            <p>4</p>
-                            <IoHeart />
-                        </span>
-                        <span className="flex gap-[5px] items-center">
-                            <p>4</p>
-                            <p>Followers</p>
-                        </span>
-                        <span className="flex gap-[5px] items-center">
-                            <IoShareSocial className="text-[20px] text-[#FFC300]" />
-                        </span>
-                    </div> */}
                 </div>
             </div>
-            <div className="w-[95%] mb-[70px] flex flex-wrap gap-[20px] max-[650px]:justify-center max-[650px]:w-[100%]">
+            <div className="w-[70%] mb-[70px] flex flex-wrap justify-center gap-[20px] max-[650px]:justify-center max-[650px]:w-[100%]">
                 {
                     allProduct?.map((i: IProduct) => (
                         <div className="w-[200px]rounded-[8px] border flex flex-col items-center p-[10px] gap-[10px] max-[650px]:w-[150px] rounded-[8px]">
