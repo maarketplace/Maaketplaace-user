cd2560eccdad824c95096b10b605c39089136224
Youngmentor <olaleresuliton43@gmail.com>
2024-08-21 01:22:36 +0100

    modify the height again

--- src/components/pages/quicks/index.tsx
+++ src/components/pages/quicks/index.tsx
@@ -79,15 +79,15 @@ const Quicks = () => {
                             spaceBetween={30}
                             mousewheel={true}
                             modules={[Mousewheel, Pagination]}
-                            className='w-[100%] h-[80vh] mt-[20px] max-[650px]:h-[85vh] max-[650px]:mt-[0px]'>
+                            className='w-[100%] h-[80vh] mt-[20px] max-[375px]:h-[80vh] max-[650px]:h-[85vh] max-[650px]:mt-[0px]'>
                             {
                                 allProduct?.map((i: IProduct) => (
                                     <SwiperSlide key={i?._id} style={{ display: 'flex', height: '100%', gap: 20, justifyContent: 'center', overflow: 'hidden' }}>
                                         <div className="w-[40%] max-[650px]:w-[100%]" >
                                             <img src={i?.productImage} alt="" className="relative w-full h-full object-cover " />
                                             <div className=" w-[100%] h-[100%] bg-[#00000038] opacity-100 flex items-end absolute top-0 left-0 right-0 bottom-0">
-                                                <div className="w-[100%] h-[400px] hidden max-[650px]:flex max-[650px]:flex-col max-[650px]:gap-[10px]">
-                                                    <div className="w-[100%] h-[80%] flex justify-end ">
+                                                <div className="w-[100%] max-[375px]:h-[500px] hidden max-[650px]:flex max-[650px]:flex-col max-[650px]:gap-[10px]">
+                                                    <div className="w-[100%] max-[375px]:h-[60%] flex justify-end ">
                                                         <div className="w-[20%] h-[100%] flex flex-col items-center justify-center gap-[10px]">
                                                             <span className="gap-[10px] w-[40px] flex items-center justify-center relative ">
                                                                 {
@@ -123,7 +123,7 @@ const Quicks = () => {
                                                             </span>
                                                         </div>
                                                     </div>
-                                                    <div className="w-[100%] h-[20%] flex items-center ">
+                                                    <div className="w-[100%] max-[375px]:h-[30%] flex items-center ">
                                                         <div className=" w-[90%] h-[30%]flex ml-[10px]">
                                                             <span className="flex items-center gap-[10px] w-[100%]">
                                                                 <p className="text-white text-lg font-bold shadow-md  truncate ">{i?.merchant?.business_name || i?.merchant?.fullName}</p>
