d75cb39bd4ae228ca2e4a4194803d91edbd28bea
Youngmentor <olaleresuliton43@gmail.com>
2024-12-02 21:20:21 +0100

    update the order summary design

--- src/components/pages/orderSummary/index.tsx
+++ src/components/pages/orderSummary/index.tsx
@@ -40,38 +40,27 @@ const OrderSummary = () => {
     };
     return (
         <div className="w-full h-[90vh] flex flex-col items-center justify-center gap-[20px] ">
-            <div>
-                <p className="text-center">Hi {user?.fullName} thank you for your purchase </p>
+            <div className="w-[95%] h-[40px] bg-[#FFC300] text-black flex items-center justify-center">
+            <p className="text-center">{user?.fullName}</p>
+            </div>
+            <div className="w-[95%]">
+                <p className="text-center text-[14px]">Hi {user?.fullName}, Thank you for your purchase </p>
             </div>
             <div className="w-[400px]  dark:bg-black bg-[#eeeded] p-[10px] max-[650px]:w-[100%] flex flex-col items-center gap-[20px] rounded-lg">
                 <span className="flex w-full justify-between text-[12px] items-center">
                     <p>Product Name:</p>
-                    <p >{product?.productName.slice(0, 40)}..</p>
+                    <p >{product?.productName.slice(0, 30)}..</p>
                 </span>
-                <span className="flex w-full justify-between text-[14px]">
-                    <p>Price:</p>
-                    <p>{formatNumber(product?.paymentPrice)}</p>
-                </span>
-                <span className="flex w-full justify-between text-[14px]">
+                <span className="flex w-full justify-between text-[12px]">
                     <p>Price:</p>
                     <p>{formatNumber(orderSummary?.payable_amount)}</p>
                 </span>
-                <span className="flex w-full justify-between text-[14px]">
-                    <p>Product Location:</p>
-                    <p>{product?.productLocation}</p>
-                </span>
-                <span className="flex w-full justify-between text-[14px]">
-                    <p>Image:</p>
-                    <img src={product?.productImage} alt="" className="w-[60px]" />
-                </span>
-
-                <span className="flex w-full justify-between text-[14px]">
+                <span className="flex w-full justify-between text-[12px]">
                     <p>Status:</p>
                     <p>{orderSummary?.status ? capitalizeFirstLetter(orderSummary.status) : ''}</p>
                 </span>
-               
             </div>
-            <div className="w-[95%] flex flex-col">
+            <div className="w-[95%] flex flex-col gap-2">
                 <span className="flex gap-[10px]">
                     <img src={product?.productImage} alt="" className="w-[60px]" />
                     <p className="text-[12px]">{product?.productName}</p>
@@ -79,12 +68,12 @@ const OrderSummary = () => {
                 <button
                     onClick={() => downloadEbook(product)}
                     disabled={isDownloadLoading === product?._id} // Disable button if loading
-                    className={`w-[80px] h-[30px] text-[10px] mt-[10px] rounded ${isDownloadLoading === product?._id
+                    className={`w-[100px] h-[30px] text-[12px] mt-[10px] rounded ${isDownloadLoading === product?._id
                         ? "bg-gray-400 cursor-not-allowed"
                         : "bg-[#FFC300] text-black"
                         }`}
                 >
-                    {isDownloadLoading === product?._id ? "Loading..." : "Download"}
+                    {isDownloadLoading === product?._id ? "Loading..." : "Download file"}
                 </button>
                 
             </div>
